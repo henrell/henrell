504566b2af5113031594ce859a09f23378d83639
Rick <luiz.costa@simbiox.com.br>
2020-10-08 19:00:25 -0300

    implementado page de Share

---
+++ dist/js/share.js
@@ -0,0 +1,13 @@
+var paramURL = window.location.href.split(`?opt=`)[1];
+
+if(paramURL == 'spotify'){
+    window.location.href = `https://open.spotify.com/artist/6uxwjXJLdarTUoirjdeWeR?si=ZsAmzE5ISSOqhOvR_Af77g`
+}
+
+if(paramURL == 'soundcloud'){
+    window.location.href `https://soundcloud.com/henrellmusic`
+}
+
+if(paramURL == 'deezer'){
+    window.location.href = `https://www.deezer.com/br/artist/8509194`
+}
